1f56cccdcfdedd81c228c86fdb5a9d0e132337aa
K-Pro <60223648+komil-pro@users.noreply.github.com>
2022-12-11 18:59:16 +0500

    st

--- python/db/sqlite.py
+++ python/db/sqlite.py
@@ -7,8 +7,6 @@ sqlite3 sqlite_db1.db
 connection = sqlite3.connect("sqlite_db1.db") # соединение с базой данных
 cursor = connection.cursor()
 #print(connection.total_changes) # проверяем создан ли успешно объект connection
-
-
 # создание новой таблицы
 #cursor.execute("CREATE TABLE students (name TEXT, age INTEGER, phone TEXT, email TEXT)")
 # добавление данных
@@ -16,16 +14,15 @@ cursor = connection.cursor()
 #cursor.execute("INSERT INTO students VALUES ('Сухроб', 22, 98012345,'suhrob@univer.tj')")
 # получение данных
 rows = cursor.execute("SELECT name, phone, email FROM students").fetchall()
-print('-'*20)
+print('~'*30)
 print(rows)
-print('-'*20)
+print('~'*30)
 # вариант выборки с критериями
 target_student_name = "Сухроб"
 rows = cursor.execute(
     "SELECT name, age, email FROM students WHERE name = ?",
     (target_student_name,),
 ).fetchall()
-print('-'*20)
 print(rows)
-print('-'*20)
+print('~'*30)
 #connection.commit()
