5a67a9f0a23fd11bc19cf3d9963a55d950cedc81
K-Pro <60223648+komil-pro@users.noreply.github.com>
2022-12-24 19:24:48 +0500

    Create menu.py

---
+++ python/db/menu.py
@@ -0,0 +1,8 @@
+print("menu:")
+print("="*20)
+print("show - to show all products")
+print("add - to add new product")
+print("="*20)
+choice = input("please type your command:")
+if choice == "add":
+	input("Please type new product name:")
